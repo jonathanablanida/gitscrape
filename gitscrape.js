0cb07425dcb9100d667640aad892b37704e73f85
Willy Collins <wcurtiscollins@gmail.com>
2013-03-13 17:42:05 -0400

    Added command line parser
    Details:
     - Only the frame of it now
     - Added the 'optimist' parsing library

--- gitscrape.js
+++ gitscrape.js
@@ -1,7 +1,25 @@
 /**
  * Run cli
  */
+var optimist = require('optimist');
 
- exports.run = function () {
- 	console.log('Welcome to gitscrape!!');
- };
+exports.run = function () {
+	var args = getArgs();
+
+	console.log('Welcome to gitscrape!!');
+	console.log(argv);
+};
+
+/**
+ * Setup arguments
+ */
+var getArgs = function () {
+
+	argv = optimist.argv;
+	return argv;
+};
+
+var find = function (expression) {
+
+};
+
