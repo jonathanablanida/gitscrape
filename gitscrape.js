653eac1ea6daaacccee3e3dd06692fb1386d84e4
Willy Collins <wcurtiscollins@gmail.com>
2013-03-14 13:36:28 -0400

    Restructured code.
    Details:
     - Gitscraper is now wrapped in a self executed function
     - methods are added to prototype
     - CLI will be added to wrap GitScraper

--- gitscrape.js
+++ gitscrape.js
@@ -1,88 +1,106 @@
 var fs 		 = require('fs');
 var ps		 = require('child_process');
 
-/**
- * Run cli
- */
-var run = function () {
 
-    // Only require optimist when running the CLI
-    var optimist = require('optimist');
-	var args = getArgs();
+var Gitscrape = (function () {
 
-    findCommits(args._);
-
-};
-
-/**
- * Setup arguments
- */
-var getArgs = function () {
-
-	argv = optimist.argv;
-	return argv;
-};
-
-var findCommits = function (expression, options) {
-    cmd = "log --grep=" + expression;
-
-
-    if (typeof cmd === 'string') {
-        cmd = cmd.split(' ');
-    }
-
-    gitcmd = ps.spawn('git', cmd);
-    results = redirect(gitcmd);
-
-    return results
-};
-
-var redirectToScreen = function (process) {
-
-	process.stdout.on('data', function(data) {
-		console.log('' + data);
-	});
-
-	process.stderr.on('data', function(data) {
-		console.log('' + data);
-	});
-
-};
-
-/**
- * Redirect stdout and stderr streams to a js object
- */
-var redirectToObject = function (process, options) {
-
-    options = options || {};
-
-    /**
-     * Get redirection options
-     */
-    var format = options.format || 'log';
-
-    object = {
-        stdout: [],
-        stderr: []
+    var _this = this;
+    var self = function () {
+        _this.redirect = this.redirectToObject;
     };
 
-    process.stdout.on('data', function(data) {
-        object.stdout.push('' + data);
-    });
+    self.prototype = {
 
-    process.stderr.on('data', function(data) {
-        object.stderr.push('' + data);
-    });
+        init: function () {
+            _this.redirect = this.redirectToObject;
+        },
 
-    return object;
-};
+        /**
+         * Run cli
+         */
+        run: function () {
 
-/**
- * Globals
- */
- var redirect = redirectToObject;
+            // Only require optimist when running the CLI
+            var optimist = require('optimist');
+            var args = getArgs();
 
-exports.run = run;
-exports.find = findCommits;
+            findCommits(args._);
+
+        },
+
+        /**
+         * Setup arguments
+         */
+        getArgs: function () {
+
+            argv = optimist.argv;
+            return argv;
+        },
+
+        findCommits: function (expression, options) {
+            cmd = "log --grep=" + expression;
+
+
+            if (typeof cmd === 'string') {
+                cmd = cmd.split(' ');
+            }
+
+            gitcmd = ps.spawn('git', cmd);
+            results = redirect(gitcmd);
+
+            return results
+        },
+
+        redirectToScreen: function (process) {
+
+            process.stdout.on('data', function(data) {
+                console.log('' + data);
+            });
+
+            process.stderr.on('data', function(data) {
+                console.log('' + data);
+            });
+
+        },
+
+        /**
+         * Redirect stdout and stderr streams to a js object
+         */
+        redirectToObject: function (process, options) {
+
+            options = options || {};
+
+            /**
+             * Get redirection options
+             */
+            var format = options.format || 'log';
+
+            object = {
+                stdout: [],
+                stderr: []
+            };
+
+            process.stdout.on('data', function(data) {
+                object.stdout.push('' + data);
+            });
+
+            process.stderr.on('data', function(data) {
+                object.stderr.push('' + data);
+            });
+
+            return object;
+        },
+
+    };
+
+    return new self();
+
+}());
+
+var CLI = (function () {
+
+}());
+
+module.exports = Gitscrape;
 
 
